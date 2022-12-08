dedad9fd5785d4fdd1a65c3020d0feed9e8b80d3
Yun-Lan Hu <hu.yunlan1@gmail.com>
2022-12-08 13:51:02 -0500

    JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,14 @@
-const cdiv = document.querySelector('.container');
+const creatediv = document.querySelector('.container');
+
+const makeGrid = (alotofGrids) => {
+  for(let i = 0;  i < alotofGrids)
+}
+
+
+
 
 // Add 16 divs
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  cdiv.appendChild(div);
-}
+//or (let i = 1; i < 257; i++) {
+//  const div = document.createElement('div');
+//  creatediv.appendChild(div);
+//}
